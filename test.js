30bb4e31e09284a03e292c1751a4ffd614e4e946
= <=>
2024-01-12 20:29:46 +0400

    Added xiaomi devices

--- test.js
+++ test.js
@@ -1,8 +1,5 @@
 var imei = require ('./scr/imei');
 
 var IMEI= new imei();
-console.log(IMEI.isValid("860921035123120")); // returns true
 
-console.log(IMEI.device("Apple","iPhone3G")); // returns string with imei by device TAC
-
-console.log(IMEI.random()); // returns string with random imei
+console.log(IMEI.device("Xiaomi","Mi13T")); // returns string with imei by device TAC
